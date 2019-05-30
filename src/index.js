648b60e640b117463c1631c3c9db592ea4367066
syntaxval <syntaxval@users.noreply.github.com>
2019-05-30 20:51:52 +0200

    Check use case: column winner.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@
 var Board = require("./classes/Board")
 
 try {
-    var b3x3 = new Board(10)
+    var board = new Board(3)
 
-    b3x3.print()
+    board.insert("✹", "0")
+    board.insert("✹", "3")
+    board.insert("✹", "6")
 
 } catch (error) {
     // eslint-disable-next-line no-console
