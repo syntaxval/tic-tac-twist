22cf409fa4b7651a6037cff37d9e178c851b914b
syntaxval <syntaxval@users.noreply.github.com>
2019-05-30 21:20:57 +0200

    Use chaining.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ var Board = require("./classes/Board")
 try {
     var board = new Board(3)
 
-    board.insert("✹", "0")
-    board.insert("✹", "3")
-    board.insert("✹", "6")
+    board.print().insert("✹", "0")
+    board.print().insert("✹", "3")
+    board.print().insert("✹", "6")
+    board.print()
 
 } catch (error) {
     // eslint-disable-next-line no-console
